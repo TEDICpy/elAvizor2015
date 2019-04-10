0cf031c3c3d2ff533f607ab47ae4e5191738abe1
Lu Pa <lupa@inventati.org>
2019-04-10 14:45:12 -0400

    restaurados los valores originales

--- views/blocks/main_news.php
+++ views/blocks/main_news.php
@@ -1,6 +1,9 @@
 <div class="col-md-6 col-md-offset-0 col-xs-offset-1 col-xs-10">
-	<div class="widget-box">
-		<a class="twitter-timeline"  href="https://twitter.com/hashtag/EscuelasQueCaen" data-widget-id="776204052338249728">Tweets sobre #EscuelasQueCaen</a>
-        	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	</div>
+        <div class="widget-box">
+        <a class="twitter-timeline" href="https://twitter.com/TEDICpy" data-widget-id="624793840331935744">Tweets por el @TEDICpy.</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        <!--    <a class="twitter-timeline"  href="https://twitter.com/hashtag/elijopy" data-widget-id="658654805221613568">Tweets sobre #elijopy</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>-->
+
+        </div>
 </div>
+
--- views/blocks/main_reports.php
+++ views/blocks/main_reports.php
@@ -1,5 +1,5 @@
 <div class="col-md-6 col-md-offset-0 col-xs-offset-1 col-xs-10">
 	<div class="widget-box">
-		<iframe height="350" src="https://www.youtube.com/embed/APxo1vBd9uE" allowfullscreen style="border: 0px; width:100%;"></iframe>
+		<iframe height="350" src="https://www.youtube.com/embed/T4V3XxuNBrU" allowfullscreen style="border: 0px; width:100%;"></iframe>
 	</div>
 </div>
